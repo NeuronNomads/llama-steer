c6610c3fc48eada68aa3ac7511f96ad6eb078d49
Shayan Sadeghieh <shayan.sadeghieh@gmail.com>
2024-12-23 13:18:58 -0500

    clean extra line

--- backend/api/main.py
+++ backend/api/main.py
@@ -16,9 +16,8 @@ async def lifespan(app: FastAPI):
     print("Shutting down server")
 
 app = FastAPI(lifespan=lifespan)
-origins = ["*"]
 app.add_middleware(CORSMiddleware,
-                   allow_origins=origins,
+                   allow_origins=["*"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
