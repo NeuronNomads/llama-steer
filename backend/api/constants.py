494940d196284bfcb6e79073e0134bfa1ea42e23
Shayan Sadeghieh <shayan.sadeghieh@gmail.com>
2024-12-28 15:11:57 -0500

    gpt2-steer

--- backend/api/constants.py
+++ backend/api/constants.py
@@ -1,7 +1,7 @@
-LAYER = 25
+LAYER = 1
 MODEL_ID = "meta-llama/Meta-Llama-3-8B-Instruct"
 SAE_ID = "blocks.25.hook_resid_post"
 SAE_IDX = 18895 # As per https://www.neuronpedia.org/llama3-8b-it/25-res-jh/18895, this refers to the NBA and its players
 SAE_RELEASE = "llama-3-8b-it-res-jh"
-STEERING_COEFF = 25
-MAX_TOKENS = 1
+STEERING_COEFF = 50
+MAX_TOKENS = 50
